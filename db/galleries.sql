0ecb6d47df8057ce6d9ab567f330200e6ce3177f
Md Shariful Islam <94960764+shariful94@users.noreply.github.com>
2022-09-13 17:15:03 +0600

    db backuped

---
+++ db/galleries.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 13, 2022 at 05:13 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maxween`
+--
+
+--
+-- Dumping data for table `galleries`
+--
+
+INSERT INTO `galleries` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'public/galleries/iVAeuJctZlunRnZ5kgcgsHofxwO6dQN2e7dyQgHd.png', '2022-09-11 04:19:36', '2022-09-11 04:19:36'),
+(2, 'public/galleries/31w7WeXclITI6HeSnPnj4N1iEhu6aCQndrshJEVe.png', '2022-09-11 04:19:36', '2022-09-11 04:19:36'),
+(3, 'public/galleries/JF0q34M5bOxHt9rabf0dmwJCwh5FBKOdEjiXleG8.png', '2022-09-11 04:19:36', '2022-09-11 04:19:36'),
+(4, 'public/galleries/VbmunGTYhlYJZUfbcQe05QzWcE3Cy9RrairFkrTY.png', '2022-09-11 04:19:37', '2022-09-11 04:19:37'),
+(5, 'public/galleries/hMhDra4mT0T6khvy4SP22qLTWwR7sIAm2ziIUK8e.jpg', '2022-09-11 04:19:37', '2022-09-11 04:19:37'),
+(6, 'public/galleries/FYqewnqKvvBtQy1ltg6XE46Q8l5nVsNLtbiXu7Yy.jpg', '2022-09-11 04:19:37', '2022-09-11 04:19:37'),
+(7, 'public/galleries/nj0e0CBoNSTPzrtDXM3soSTj7ZwvcHBpvhMnKj7U.png', '2022-09-11 04:19:39', '2022-09-11 04:19:39'),
+(8, 'public/galleries/QgDK7qJJbtokZ28twYCeHzWi9O0RWUjAn6g7LYNg.jpg', '2022-09-11 04:19:39', '2022-09-11 04:19:39'),
+(9, 'public/galleries/mVSLNKzSWL4X4bV2wgVnDJM9UyCnMEmX7HXfCvna.jpg', '2022-09-11 04:19:39', '2022-09-11 04:19:39'),
+(10, 'public/galleries/Dbj9lFv9DQkLAVbnqw7hwZPDbxDOQCDQfot7W9Ze.png', '2022-09-11 04:19:41', '2022-09-11 04:19:41'),
+(11, 'public/galleries/jPGqFYh32c1NIKImTGrWpF8FDEfWR0eVPyKwSYLY.png', '2022-09-11 04:19:42', '2022-09-11 04:19:42'),
+(12, 'public/galleries/lU50suWjXzjuFUL3ca16zTL6S9MXFdiphmycuLhi.png', '2022-09-11 04:19:42', '2022-09-11 04:19:42'),
+(13, 'public/galleries/bdqFdurmwEnxVolUKXd5xqPTaaTZj1YNvbhcBjsr.jpg', '2022-09-11 04:19:42', '2022-09-11 04:19:42'),
+(14, 'public/galleries/kAzyeL8WdfziJpK8FH8ItbOBFARkP6zfYIAVNipF.jpg', '2022-09-11 04:19:42', '2022-09-11 04:19:42'),
+(15, 'public/galleries/vZEQCkhwcVfqaXdK58en1Iskl6GGqtq4jeTH8u0l.jpg', '2022-09-11 04:19:42', '2022-09-11 04:19:42'),
+(16, 'public/galleries/yJly50R25BjxigZ99wCMZsy8Y46EngyZ14MAbCRI.jpg', '2022-09-11 04:19:43', '2022-09-11 04:19:43'),
+(17, 'public/galleries/4fMml5aZZK4jYHcUJx1zSIMxRfkWCUqK9MeHoCAM.jpg', '2022-09-11 04:19:43', '2022-09-11 04:19:43'),
+(18, 'public/galleries/mwBPImTQhdhRtoX7HrfPavfFkPVQgB2aFxEGvV8G.jpg', '2022-09-11 04:19:43', '2022-09-11 04:19:43'),
+(19, 'public/galleries/giDiXFI7PZLPI0yYZWPn6KclFxh9t6PavKIIUXK5.jpg', '2022-09-11 04:19:43', '2022-09-11 04:19:43'),
+(20, 'public/galleries/R8PhyAMcSLoXbmpqtckCxyUl5bBOJNd52vXC4DJP.jpg', '2022-09-11 04:19:43', '2022-09-11 04:19:43');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ db/productimages.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 13, 2022 at 05:12 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maxween`
+--
+
+--
+-- Dumping data for table `productimages`
+--
+
+INSERT INTO `productimages` (`id`, `product_id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 1, 'public/products/fAvZcuzSNNkng844iVoyMmchZ45CMoTyxhfy392n.png', '2022-09-11 03:27:43', '2022-09-11 03:27:43'),
+(2, 2, 'public/products/wsPUMESZOamrFkVnVgFMfE6dRKz8POjJ78BZ9IvL.png', '2022-09-11 03:31:01', '2022-09-11 03:31:01'),
+(3, 2, 'public/products/imMG6HCEutjQbjF7jlMtESomKAwV4JxbIL3T1xvV.png', '2022-09-11 03:31:01', '2022-09-11 03:31:01'),
+(4, 2, 'public/products/qZpjtMDClIayrYI2gzfX5PrnGf66j2PDsoVNfolU.png', '2022-09-11 03:31:01', '2022-09-11 03:31:01'),
+(5, 2, 'public/products/pEXGzveHOpgslt85kpdPTeDOqKMxJQlMSBl9RAs5.png', '2022-09-11 03:31:02', '2022-09-11 03:31:02'),
+(6, 3, 'public/products/DcQ7IdaqDuj5AHglKn0yJW32uphQASyzfFh63WC0.jpg', '2022-09-11 03:35:58', '2022-09-11 03:35:58'),
+(7, 4, 'public/products/Wr8PUtmbLl8GyuoRi3N1wvozNdXA0flwaE8CU2Ty.png', '2022-09-11 03:41:22', '2022-09-11 03:41:22'),
+(8, 5, 'public/products/OPkBKqTpDBDf5U1whg1JJa5Wvq6FVT6GyH8rMzWl.jpg', '2022-09-11 03:52:38', '2022-09-11 03:52:38'),
+(9, 5, 'public/products/kIP8QhKF15FkLEelJjWn6oBpXFchL3Eog74I3Ok6.jpg', '2022-09-11 03:52:38', '2022-09-11 03:52:38'),
+(10, 6, 'public/products/96u1WNmnN9Tz5nNWI9GgKEgelg6xPG3zDruYslzl.jpg', '2022-09-11 03:57:33', '2022-09-11 03:57:33'),
+(11, 6, 'public/products/7tXxB7eUWkDJ4x8RWIW4YG8KCRd2aT30GQPJQtIG.jpg', '2022-09-11 03:57:33', '2022-09-11 03:57:33'),
+(12, 6, 'public/products/J1RVVgaj8wgSvlal9JS4Ggt74sKJWS6gc0XuP2vm.jpg', '2022-09-11 03:57:34', '2022-09-11 03:57:34'),
+(13, 6, 'public/products/TtbvvJLMp43d3EAvfYTNNlQyveVT8IiBBKWK6Lve.jpg', '2022-09-11 03:57:34', '2022-09-11 03:57:34'),
+(14, 7, 'public/products/BusPFQ5uwqJcL4ioQ80jakx7HlLx6lvp6pmgZ6l7.jpg', '2022-09-11 04:00:25', '2022-09-11 04:00:25'),
+(15, 8, 'public/products/8rAvUcqKHB0xT3bAP64N2TXJY2nMGckQUiSK8U9O.jpg', '2022-09-11 04:35:28', '2022-09-11 04:35:28'),
+(16, 8, 'public/products/XDi9ZcLF6gopz9Z5omqb5vgCArl783xQpff9hI4S.jpg', '2022-09-11 04:35:28', '2022-09-11 04:35:28');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
